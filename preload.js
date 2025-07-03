2b37e546e554d5c5f5bd38cca1157edf5b5cafc6
Mert Dogu <mertdoguonder@protonmail.com>
2025-07-03 17:10:39 +0300

    feat: relocated

--- preload.js
+++
@@ -1,9 +0,0 @@
-// preload.js
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('myApi', {
-    send: (channel, data) => ipcRenderer.send(channel, data),
-    receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-    getSystemTheme: () => ipcRenderer.invoke('get-system-theme')
-
-});
